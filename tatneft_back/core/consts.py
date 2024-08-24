d7546fb148d428fcdfe89fd24714d3606a00d6a5
Ivan122727 <ermolovivan2018@gmail.com>
2024-08-24 13:25:00 +0300

    back

--- tatneft_back/core/consts.py
+++ tatneft_back/core/consts.py
@@ -6,3 +6,4 @@ from tatneft_back.core.settings import Settings
 settings = Settings()
 db = DB(mongo_uri=settings.mongo_uri, mongo_db_name=settings.mongo_db_name)
 db.drop_collections(only_using=False)
+db.drop_collections(only_using=False)
